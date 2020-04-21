c4693ec3ab188e2db7a37e189e72ec7a0d115d6f
apt <jorge.teixeira@sage.com>
2020-04-21 12:56:53 +0100

    New custom tab in customers

--- Sage50c.Extensibility.Sample/Sage50c.Extensibility.Sample/CustomerHandler.cs
+++ Sage50c.Extensibility.Sample/Sage50c.Extensibility.Sample/CustomerHandler.cs
@@ -118,8 +118,8 @@ namespace Sage50c.ExtenderSample {
                 managementConsole = (IManagementConsole)propertyList.get_Value("IManagementConsole");
 
                 // Form a colocar no TAB dos clientes
-                //formProps = new FormProps();
-                //managementConsole.AddChildPanel(formProps);
+                var formProps = new FormProps();
+                managementConsole.AddChildPanel(formProps);
             }
 
             // Acrescentar Items ao menu
