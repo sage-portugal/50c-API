74fe3ea055e2b717499166c0f0a66953f185c02d
apt <jorge.teixeira@sage.com>
2023-06-15 23:50:31 +0100

    Added new applicaion icon

--- Sage50c.API.Sample/Sage50c.API.Sample/Properties/Resources.Designer.cs
+++ Sage50c.API.Sample/Sage50c.API.Sample/Properties/Resources.Designer.cs
@@ -61,6 +61,16 @@ namespace Sage50c.API.Sample.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon S_ico_Black {
+            get {
+                object obj = ResourceManager.GetObject("S_ico_Black", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap Sage_logo {
