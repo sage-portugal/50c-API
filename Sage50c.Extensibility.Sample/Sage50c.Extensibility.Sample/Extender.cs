36396bffe06f4b9b57e088c653fc605efcf180a9
Hugo Carvalho <hugo.carvalho@sage.com>
2023-11-29 11:23:35 +0000

    Adds an handler for extra functionality on the supplier window

--- Sage50c.Extensibility.Sample/Sage50c.Extensibility.Sample/Extender.cs
+++ Sage50c.Extensibility.Sample/Sage50c.Extensibility.Sample/Extender.cs
@@ -1,4 +1,5 @@
 ﻿using S50cBL22;
+using Sage50c.ExtenderSample22.Handlers;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -8,17 +9,18 @@ using System.Text;
 namespace Sage50c.ExtenderSample22 {
     [ProgId("Sage50c.ExtenderSample22")]
     public class Extender : ISageExtender, IDisposable {
-        private SystemHandler           systemHandler = null;               // System handler, startup, system menus
-        private SystemInfoHandler       systemInfoHandler = null;           // Parâmetros do sistema
-        private WorkstationInfoHandler  workstationInfoHandler = null;      // Configuração de postos
+        private SystemHandler systemHandler = null;                   // System handler, startup, system menus
+        private SystemInfoHandler systemInfoHandler = null;           // Parâmetros do sistema
+        private WorkstationInfoHandler workstationInfoHandler = null; // Configuração de postos
 
         private AccountTransactionHandler accountTransactionHandler = null; //AccountTransactionHandler
-        private TransactionHandler      transactionHandler = null;          // Sales Transaction handler
-        private TransactionHandler      buyTransactionHandler = null;       // Purchases Transaction handler
+        private TransactionHandler transactionHandler = null;               // Sales Transaction handler
+        private TransactionHandler buyTransactionHandler = null;            // Purchases Transaction handler
         private TenderTransactionHandler tenderTransactionHandler = null;   // Tender Transaction handler
-        private StockHandler            stockHandler = null;                // StockTransaction handler
-        private ItemHandler             itemHandler = null;                 // Items
-        private CustomerHandler         customerHandler = null;             // Customer
+        private StockHandler stockHandler = null;                           // StockTransaction handler
+        private ItemHandler itemHandler = null;                             // Items
+        private CustomerHandler customerHandler = null;                     // Customer
+        private SupplierHandler supplierHandler = null;                     // Supplier
 
         public string Initialize(string ApplicationKey) {
             //Do Nothing for now
@@ -94,8 +96,7 @@ namespace Sage50c.ExtenderSample22 {
                     break;
 
                 case "stocktransactiondetail":
-                    if (stockHandler == null)
-                    {
+                    if (stockHandler == null) {
                         stockHandler = new StockHandler();
                     }
                     stockHandler.SetDetailEventsHandler(EventHandler);
@@ -106,6 +107,13 @@ namespace Sage50c.ExtenderSample22 {
                         tenderTransactionHandler = new TenderTransactionHandler();
                     }
                     tenderTransactionHandler.SetHeaderEventsHandler(EventHandler);
+                    break;
+
+                case "supplier":
+                    if (supplierHandler == null) {
+                        supplierHandler = new SupplierHandler();
+                    }
+                    supplierHandler.SetEventHandler(EventHandler);
                     break;
 
                 //case "confstores":  // delegações
@@ -125,12 +133,10 @@ namespace Sage50c.ExtenderSample22 {
                         workstationInfoHandler = new WorkstationInfoHandler();
                     }
                     workstationInfoHandler.SetEventHandler(EventHandler);
-
                     break;
 
                 default:
                     break;
-
             }
         }
 
@@ -147,7 +153,7 @@ namespace Sage50c.ExtenderSample22 {
                 systemHandler.Dispose();
                 systemHandler = null;
             }
-            if( itemHandler != null) {
+            if (itemHandler != null) {
                 itemHandler.Dispose();
                 itemHandler = null;
             }
