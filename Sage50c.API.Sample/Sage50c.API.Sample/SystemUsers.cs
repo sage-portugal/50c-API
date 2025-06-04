523dc6aefd5405d68920c51f2eb46f375a1078e5
João Costa <joaomacostadev@gmail.com>
2025-06-04 11:48:26 +0100

    Add SystemUsers user control and UI refinements
    Introduced a new SystemUsers user control to manage and display system users in JSON format. Added related files (`SystemUsers.cs`, `SystemUsers.Designer.cs`, `SystemUsers.resx`) and updated `fAPI.Designer.cs` to include the control in a new tab. Refined UI layout for consistency and compactness, adjusted localization, and removed redundant elements. Made `SystemManager` property public in `APIEngine.cs` for external access. Removed `BReadCurrentDocumentJSON_Click` method and added necessary namespaces for JSON handling.

---
+++ Sage50c.API.Sample/Sage50c.API.Sample/SystemUsers.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using S50cSys22;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sage50c.API.Sample
+{
+    public partial class SystemUsers : UserControl
+    {
+        public SystemUsers()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Returns a list of system users.
+        /// </summary>
+        /// <returns>The list of system users.</returns>
+        private static List<SystemUser> GetSystemUsers()
+        {
+            var systemUsers = new List<SystemUser>();
+
+            APIEngine.SystemManager.Users.OfType<SystemUser>().ToList().ForEach(fe =>
+            {
+                systemUsers.Add(fe);
+            });
+
+            return systemUsers;
+        }
+
+        private void BGetSystemUsers_Click(object sender, EventArgs e)
+        {
+            var systemUsers = GetSystemUsers();
+
+            TbSystemUsersList.Text = JsonConvert.SerializeObject(systemUsers, Formatting.Indented);
+        }
+    }
+}
